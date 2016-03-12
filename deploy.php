d92f6a342c596d7e93c3bc311f551d043606ede5
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2016-03-12 16:41:09 +0000

    Add iterations to arguments for deploy script

--- deploy.php
+++ deploy.php
@@ -1,14 +1,19 @@
 <?php
 
-if (!isset($_SERVER['SHELL'])) {
+if (!isset($_SERVER['argc']) || $_SERVER['argc'] === 0) {
     http_response_code(500);
     echo 'This script must be run in the shell';
     exit();
+}
+
+$iterations = 0;
+if ($_SERVER['argc'] > 1) {
+    $iterations = $_SERVER['argv'][1];
 }
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $container = new \RoundPartner\Deploy\Container();
 
-$server = \RoundPartner\Deploy\Server\ServerFactory::multiRun($container, 1000);
+$server = \RoundPartner\Deploy\Server\ServerFactory::multiRun($container, $iterations);
 $server->dispatch();
