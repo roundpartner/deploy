8a9ed7a49cfb47e4cfe23a5425bf586c16bfa678
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2016-04-08 12:55:38 +0100

    Adding return values to comments

--- tests/Mocks/Message.php
+++ tests/Mocks/Message.php
@@ -4,16 +4,23 @@ namespace RoundPartner\Test\Mocks;
 
 class Message extends \RoundPartner\Cloud\Message
 {
+    
     function __construct($message)
     {
         parent::__construct($message, false);
     }
-    
+
+    /**
+     * @return mixed
+     */
     public function getBody()
     {
         return $this->message;
     }
 
+    /**
+     * @return bool
+     */
     public function delete()
     {
         return true;
